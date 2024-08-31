e79d02016a66e2fa783ef337b011aec5e7c3f2d8
klausmonga <monga.wakowa@gmail.com>
2024-08-31 13:51:36 +0200

    add code

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import time
+import os
+import json
+import random
+import multiprocessing
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def Onstart(extra_bundel):
+    b = 0
+    pr1_trigger = extra_bundel['remote_params']['pr1_trigger']
+    pr2_trigger = extra_bundel['remote_params']['pr2_trigger']
+    pr3_trigger = extra_bundel['remote_params']['pr3_trigger']
+    os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch1/power -m 1")
+    while 1:
+        # print(b)
+        time.sleep(7)
+        random.random()
+        pr1 = random.randint(0, 10)
+        pr2 = random.randint(0, 10)
+        pr3 = random.randint(0, 10)
+        temp1 = random.randint(0, 100)
+        temp2 = random.randint(0, 100)
+        temp3 = random.randint(0, 100)
+        os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch1/temp -m " + str(temp1))
+        os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch2/temp -m " + str(temp2))
+        os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch3/temp -m " + str(temp3))
+        os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch1/pression -m " + str(pr1))
+        os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch2/pression -m " + str(pr2))
+        os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch3/pression -m " + str(pr3))
+        if pr1 > pr1_trigger:
+            os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch1/power -m 0")
+        else:
+            os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch1/power -m 1")
 
+        if pr2 > pr2_trigger:
+            os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch2/power -m 0")
+        else:
+            os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch2/power -m 1")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if pr3 > pr3_trigger:
+            os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch3/power -m 0")
+        else:
+            os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch3/power -m 1")
